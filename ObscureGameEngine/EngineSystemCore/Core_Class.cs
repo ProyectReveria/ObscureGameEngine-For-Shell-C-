6a29cbf2b3800e0fca3d283e21188d3693cce4e5
P.Revery By Uriel "Rosary" Gomez <Brokenlevenheart@gmail.com>
2026-05-25 18:34:49 -0700

    A veces odio github
    si, es un parche SOLO para mandar 3 cambios tontos.

--- ObscureGameEngine/EngineSystemCore/Core_Class.cs
+++ ObscureGameEngine/EngineSystemCore/Core_Class.cs
@@ -21,6 +21,10 @@ class Screen_Render_Sceen_Hitbox : Math_Alogirth
 {
 
     public const int Map_Limitx = 16, Map_Limity = 16;
+
+    public static  Vector2 floor_limit; 
+    public static  Vector2 Seal_limit; 
+
     public static int StarterPointAx = 16, StarterPointAy = 16; //For the cube
     public static int StarterPointBx = 16, StarterPointBy = 16;//for the cube
     public static int _HitBox_PlayerLimit_on_X = Map_Limitx  , _HitBox_PlayerLimit_On_Y = Map_Limity;
