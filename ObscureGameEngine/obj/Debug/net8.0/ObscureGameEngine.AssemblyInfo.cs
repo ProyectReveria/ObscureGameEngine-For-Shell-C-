0bbaa9e20ddac2e58be992e613ef4a69a30317f8
P.Revery By Uriel "Rosary" Gomez <Brokenlevenheart@gmail.com>
2026-05-28 02:59:02 -0700

    OE 2.2 (NOW WHIT KERNEL!)
    Eh actualziado a Diana solo un poco, ahora deberia ser posible correr el engine en una base fija de 16 bits, ni uno mas ni uno menos con solo activarlo, also ahora deberia poderse correr como si estuviera en unity usando "Update()".
    
    Yep, diana come bien hoy

--- ObscureGameEngine/obj/Debug/net8.0/ObscureGameEngine.AssemblyInfo.cs
+++ ObscureGameEngine/obj/Debug/net8.0/ObscureGameEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObscureGameEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24dd85cbb225031bd1e1cb228fb74de0da3e63cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72ffead306aa1f67234ec18deb803209a246d52d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObscureGameEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObscureGameEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
