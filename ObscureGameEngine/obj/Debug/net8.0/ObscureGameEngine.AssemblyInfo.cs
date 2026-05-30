abd4ed0604758c55a4c6ddc5ceb4195b439df55d
P.Revery By Uriel "Rosary" Gomez <Brokenlevenheart@gmail.com>
2026-05-30 10:12:34 -0700

    Deserializacion de las World Configs

--- ObscureGameEngine/obj/Debug/net8.0/ObscureGameEngine.AssemblyInfo.cs
+++ ObscureGameEngine/obj/Debug/net8.0/ObscureGameEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObscureGameEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bbaa9e20ddac2e58be992e613ef4a69a30317f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86f55993891ee1b166e9c5d044c4a591343ecc14")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObscureGameEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObscureGameEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
